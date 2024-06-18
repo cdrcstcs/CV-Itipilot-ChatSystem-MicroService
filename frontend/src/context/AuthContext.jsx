c59bed6e3235045ba70373d8be1367684c7d5cb6
hieu <hieushinnosuke@gmail.com>
2024-06-18 21:23:18 +0800

    add

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,14 +1,26 @@
 import { createContext, useContext} from "react";
-import { useCookies } from "./Cookies";
 import jwt from "jsonwebtoken";
 export const AuthContext = createContext();
 export const useAuthContext = () => {
 	return useContext(AuthContext);
 };
+function getCookie(name) {
+    // Construct the regular expression to match the cookie name and value
+    const cookieRegex = new RegExp('(^|;\\s*)(' + name + ')=([^;]*)');
+    const cookieMatch = document.cookie.match(cookieRegex);
+
+    // If the cookie is found, return its value (decoded)
+    if (cookieMatch) {
+        return decodeURIComponent(cookieMatch[3]);
+    } else {
+        return null; // Return null if cookie with specified name is not found
+    }
+}
+
 export const AuthContextProvider = ({ children }) => {
 	const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 	let authUser = null;
-	jwt.verify(useCookies().get('usertoken'), jwtSecret, {}, (err, userData) => {
+	jwt.verify(getCookie('usertoken'), jwtSecret, {}, (err, userData) => {
 		if (err) {
 			console.log(err);
 		} else {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
-import CookiesProvider from "./context/Cookies.jsx";
 import { SocketContextProvider } from "./context/SocketContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<CookiesProvider>
-			<AuthContextProvider>
-				<SocketContextProvider>
-					<App />
-				</SocketContextProvider>
-			</AuthContextProvider>
-		</CookiesProvider>
+		<AuthContextProvider>
+			<SocketContextProvider>
+				<App />
+			</SocketContextProvider>
+		</AuthContextProvider>
 	</React.StrictMode>
 );
